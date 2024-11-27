2719b394267857c3800bdfa2e42c02c0edc9db4f
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-11-28 02:08:42 +0800

    Update Person.java
    Simple properties (ID, name, contact number) are created, two constructors are added, and setters and getters are added. We're ready to add data validation methods and exception throwing

--- ZhanboGong_A2/src/com/themepark/management/entity/Person.java
+++ ZhanboGong_A2/src/com/themepark/management/entity/Person.java
@@ -6,10 +6,46 @@ public class Person {
     protected String lastName;
     protected String mobileNumber;
 
+    public Person(){}
+
     public Person(int ID, String firstName, String lastName, String mobileNumber) {
         this.ID = ID;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.mobileNumber = mobileNumber;
+    }
+
+    // Note: Use regular expressions to validate your data, and throw exceptions instead of printing them
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
         this.mobileNumber = mobileNumber;
     }
 }
