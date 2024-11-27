e04f697818bee632c7f891cb2766bf172eaabc4e
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-11-28 02:21:24 +0800

    Create AssessmentTwo.java
    The structure of AssessmentTwo was set up as required in the assignment, and the entity package was imported

---
+++ ZhanboGong_A2/src/com/themepark/management/test/AssessmentTwo.java
@@ -0,0 +1,20 @@
+package com.themepark.management.test;
+
+import com.themepark.management.entity.*;
+public class AssessmentTwo {
+    public static void main(String[] args) {
+
+    }
+    public void partThree(){
+    }
+    public void partFourA(){
+    }
+    public void partFourB(){
+    }
+    public void partFive(){
+    }
+    public void partSix(){
+    }
+    public void partSeven(){
+    }
+}
