5cc6acd3eef41037696a795273db475793297e29
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-11-28 12:30:48 +0800

    Update Ride.java
    The property is set, and both constructors are updated, along with the setter and getter methods for each property

--- ZhanboGong_A2/src/com/themepark/management/entity/Ride.java
+++ ZhanboGong_A2/src/com/themepark/management/entity/Ride.java
@@ -2,4 +2,38 @@ package com.themepark.management.entity;
 
 public class Ride {
     // rollerCoaster carries 2 people at a time and enters history after playing, while thunderstorm carries 4 people at a time
+    private String rideType;
+    private Employee employee;
+    private boolean operatingState;
+
+    public Ride(){}
+    public Ride(String rideType, Employee employee, boolean operatingState) {
+        this.rideType = rideType;
+        this.employee = employee;
+        this.operatingState = operatingState;
+    }
+
+    public String getRideType() {
+        return rideType;
+    }
+
+    public void setRideType(String rideType) {
+        this.rideType = rideType;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public boolean isOperatingState() {
+        return operatingState;
+    }
+
+    public void setOperatingState(boolean operatingState) {
+        this.operatingState = operatingState;
+    }
 }
