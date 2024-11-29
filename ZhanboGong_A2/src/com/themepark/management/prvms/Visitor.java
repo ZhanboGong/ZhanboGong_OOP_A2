1b077d64cd4a4958a185bae7899ce21737f1380f
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-11-29 23:32:31 +0800

    Update Visitor.java
    A unique Visitor attribute was added, two constructors were created, and setter and getter methods were provided

--- ZhanboGong_A2/src/com/themepark/management/prvms/Visitor.java
+++ ZhanboGong_A2/src/com/themepark/management/prvms/Visitor.java
@@ -1,4 +1,32 @@
 package com.themepark.management.prvms;
 
+import java.util.ArrayList;
+
 public class Visitor {
+
+    private String playStatus;
+    private int playOfNumber = 1;
+    private final ArrayList<String> rideHistories = new ArrayList<>();
+
+    public Visitor(){super();}
+    public Visitor(int ID, String firstName, String lastName, String mobileNumber, String playStatus){
+        super();
+        this.playStatus = playStatus;
+    }
+
+    public String getPlayStatus() {
+        return playStatus;
+    }
+
+    public void setPlayStatus(String playStatus) {
+        this.playStatus = playStatus;
+    }
+
+    public int getPlayOfNumber() {
+        return playOfNumber;
+    }
+
+    public ArrayList<String> getRideHistories() {
+        return rideHistories;
+    }
 }
