e0d4f1c5c03d1c251724a6d88b8795ab04d92b03
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-11-29 22:25:23 +0800

    Update Person.java
    Add data validation to the setter methods for each property, using regular expressions for data validation

--- ZhanboGong_A2/src/com/themepark/management/prvms/Person.java
+++ ZhanboGong_A2/src/com/themepark/management/prvms/Person.java
@@ -30,22 +30,52 @@ public abstract class Person {
     }
 
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        String firstNameRegex = "[A-Z][a-z]*";
+        if(firstName == null || firstName.trim().isEmpty()){
+            System.out.println("To be written...");
+        } else if (!firstName.matches(firstNameRegex)) {
+            System.out.println("The first name must begin with an uppercase letter and follow with a lowercase letter!");
+        }
+        else {
+            this.firstName = firstName;
+        }
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        String lastNameRegex = "[A-Z][a-z]*";
+        if(lastName == null || lastName.trim().isEmpty()){
+            System.out.println("To be written...");
+        } else if (!lastName.matches(lastNameRegex)) {
+            System.out.println("The last name must begin with an uppercase letter and follow with a lowercase letter!");
+        }
+        else {
+            this.lastName = lastName;
+        }
     }
 
     public String getMobileNumber() {
         return mobileNumber;
     }
 
+    //After searching on the Internet, the format of landline telephones in Australia is (0X) XXXX XXXX,
+    // and the format of mobile telephones is 04XX XXX XXX,
+    // so regular expressions are used to verify the data of the phone number entered or updated
     public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
+        String mobileRegex = "0\\d[ -]?\\d{2}[ -]?\\d{2}[ -]?\\d[ -]?\\d{3}";
+        if(mobileNumber == null || mobileNumber.trim().isEmpty())
+        {
+            System.out.println("Wrong Input: Mobile number cannot be empty and null!");
+        }
+        else if (!mobileNumber.matches(mobileRegex)) {
+            System.out.println("Wrong Input: Phone numbers are nine or ten digits long!");
+        }
+        else{
+            this.mobileNumber = mobileNumber;
+        }
     }
 }
