7c2abf8ddf4ff08dc64b94338321289485224c46
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-11-29 23:02:18 +0800

    Update Employee.java
    You set Employee to be a subclass of Person, creating two new properties, type and status, as well as two constructors that don't provide setters and getters for each subclass property

--- ZhanboGong_A2/src/com/themepark/management/prvms/Employee.java
+++ ZhanboGong_A2/src/com/themepark/management/prvms/Employee.java
@@ -1,4 +1,29 @@
 package com.themepark.management.prvms;
 
-public class Employee {
+public class Employee extends Person{
+    private String employeeType;
+
+    private boolean workingStatus;
+    public Employee(){super();}
+    public Employee(int ID, String firstName, String lastName, String mobileNumber, String employeeType, boolean workingStatus){
+        super(ID, firstName, lastName, mobileNumber);
+        this.employeeType = employeeType;
+        this.workingStatus = workingStatus;
+    }
+
+    public String getEmployeeType() {
+        return employeeType;
+    }
+
+    public void setEmployeeType(String employeeType) {
+        this.employeeType = employeeType;
+    }
+
+    public boolean isWorkingStatus() {
+        return workingStatus;
+    }
+
+    public void setWorkingStatus(boolean workingStatus) {
+        this.workingStatus = workingStatus;
+    }
 }
