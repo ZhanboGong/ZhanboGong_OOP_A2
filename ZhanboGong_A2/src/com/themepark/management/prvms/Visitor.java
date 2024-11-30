5ba269e10a5a1b1d339405ab79d0f03fc73b9cae
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-11-30 20:10:11 +0800

    Update Visitor.java
    Added the attribute ticketType and added data validation for other attributes, using regular expressions so that ticketType and playStatus must be in the specified string

--- ZhanboGong_A2/src/com/themepark/management/prvms/Visitor.java
+++ ZhanboGong_A2/src/com/themepark/management/prvms/Visitor.java
@@ -5,13 +5,15 @@ import java.util.ArrayList;
 public class Visitor {
 
     private String playStatus;
-    private int playOfNumber = 1;
+    private String ticketType;
     private final ArrayList<String> rideHistories = new ArrayList<>();
+    private int playOfNumber = 1;
 
     public Visitor(){super();}
-    public Visitor(int ID, String firstName, String lastName, String mobileNumber, String playStatus){
+    public Visitor(int ID, String firstName, String lastName, String mobileNumber, String playStatus, String ticketType){
         super();
         this.playStatus = playStatus;
+        this.ticketType = ticketType;
     }
 
     public String getPlayStatus() {
@@ -19,11 +21,35 @@ public class Visitor {
     }
 
     public void setPlayStatus(String playStatus) {
-        this.playStatus = playStatus;
+        String playStatusRegex = "Leisure|On Queue|On Roller Coaster|On Thunder Storm";
+        if(!playStatus.matches(playStatusRegex)){
+            System.out.println("Wrong Input: The play state must be Leisure, On Queue, On Roller Coaster, or On Thunder Storm!");
+        }
+        else{
+            this.playStatus = playStatus;
+        }
+    }
+
+    public String getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(String ticketType) {
+        String ticketTypeRegex = "Children|Adult|Elder";
+        if(!ticketType.matches(ticketTypeRegex)){
+            System.out.println("Wrong Input: The ticket type must be Children, Adult or Elder!");
+        }
+        else{
+            this.ticketType = ticketType;
+        }
     }
 
     public int getPlayOfNumber() {
         return playOfNumber;
+    }
+
+    public void setPlayOfNumber(){
+        this.playOfNumber++;
     }
 
     public ArrayList<String> getRideHistories() {
