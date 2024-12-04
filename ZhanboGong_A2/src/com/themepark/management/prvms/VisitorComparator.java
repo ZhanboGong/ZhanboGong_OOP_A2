1e8176dc53eaee0734878b2d69d57719b11b2add
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-12-04 17:40:16 +0800

    Create VisitorComparator.java
    Just create, finish Part4B

---
+++ ZhanboGong_A2/src/com/themepark/management/prvms/VisitorComparator.java
@@ -0,0 +1,10 @@
+package com.themepark.management.prvms;
+
+import java.util.Comparator;
+
+public class VisitorComparator implements Comparator<Visitor> {
+    @Override
+    public int compare(Visitor visitor1, Visitor visitor2){
+        return 1;
+    }
+}
