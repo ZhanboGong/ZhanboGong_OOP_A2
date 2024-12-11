a2ef577040fe2b6de5dc5739a6e3a8743282960d
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-12-11 15:10:15 +0800

    Update VisitorComparator.java
    The Comparator method is implemented and the compare method is overridden. My logic is to check the ID first, and if the ID is the same, then check the visitor's lastName

--- ZhanboGong_A2/src/com/themepark/management/prvms/VisitorComparator.java
+++ ZhanboGong_A2/src/com/themepark/management/prvms/VisitorComparator.java
@@ -4,7 +4,13 @@ import java.util.Comparator;
 
 public class VisitorComparator implements Comparator<Visitor> {
     @Override
-    public int compare(Visitor visitor1, Visitor visitor2){
-        return 1;
+    public int compare(Visitor visitor1, Visitor visitor2) {
+        if (visitor1.getID() < visitor2.getID()) {
+            return -1;
+        } else if (visitor1.getID() > visitor2.getID()) {
+            return 1;
+        } else {
+            return visitor1.getLastName().compareTo(visitor2.getLastName());
+        }
     }
 }
