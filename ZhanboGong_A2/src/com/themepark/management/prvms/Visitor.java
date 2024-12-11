a34580087fa1ea0eaa0fbc97438165d950bb27d1
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-12-11 14:32:20 +0800

    Update Visitor.java
    The Visitor class is commented in detail, see the code for details

--- ZhanboGong_A2/src/com/themepark/management/prvms/Visitor.java
+++ ZhanboGong_A2/src/com/themepark/management/prvms/Visitor.java
@@ -2,6 +2,11 @@ package com.themepark.management.prvms;
 
 import java.util.ArrayList;
 
+/**
+ * The Visitor class inherits from the abstract class Person
+ * The attribute playStatus, ticketType, rideHistories and playOfNumber belonging to Visitor is extended
+ * It provides empty and parameter constructors, as well as getters and setters to validate and read data.
+ */
 public class Visitor extends Person{
 
     private String playStatus;
@@ -9,13 +14,27 @@ public class Visitor extends Person{
     private final ArrayList<String> rideHistories = new ArrayList<>();
     private int playOfNumber = 1;
 
+    // No-argument constructors
     public Visitor(){super();}
+
+    /**
+     *
+     * @param ID: Represents the ID of the Visitor(Person)
+     * @param firstName: Represents the firstName of the Visitor(Person)
+     * @param lastName: Represents the lastName of the Visitor(Person)
+     * @param mobileNumber: Represents the Contact Phone Number of the Visitor(Person)
+     * @param playStatus: Represents the playStatus of the Visitor
+     * @param ticketType: Represents the ticketType of the Visitor
+     */
     public Visitor(int ID, String firstName, String lastName, String mobileNumber, String playStatus, String ticketType){
         super(ID, firstName, lastName, mobileNumber);
         this.playStatus = playStatus;
         this.ticketType = ticketType;
     }
 
+    /**
+     * his is an abstract method on the Person class that prints information about the object
+     */
     @Override
     public void printInformation(){
         System.out.println("================================");
@@ -30,15 +49,17 @@ public class Visitor extends Person{
     public void personalHistory(String rideType){
         String personalRecord = playOfNumber++ + ":" + rideType;
         rideHistories.add(personalRecord);
-        System.out.println("反馈");
+        System.out.println("Visitor" + ID + " added success to his or her personal history");
     }
 
     // 可添加方法进行读取历史
 
+    // Used to get the visitor play status
     public String getPlayStatus() {
         return playStatus;
     }
 
+    // Used to set the new play status and verify that the play status must be one of Leisure, On Queue, On Roller Coaster and On Thunder Storm
     public void setPlayStatus(String playStatus) {
         String playStatusRegex = "Leisure|On Queue|On Roller Coaster|On Thunder Storm";
         if(!playStatus.matches(playStatusRegex)){
@@ -49,10 +70,12 @@ public class Visitor extends Person{
         }
     }
 
+    // Used to get the ticketType
     public String getTicketType() {
         return ticketType;
     }
 
+    // Used to set the new play status and verify that the play status must be one of Children, Adult and Elder
     public void setTicketType(String ticketType) {
         String ticketTypeRegex = "Children|Adult|Elder";
         if(!ticketType.matches(ticketTypeRegex)){
@@ -63,14 +86,17 @@ public class Visitor extends Person{
         }
     }
 
+    // Used to get the play number
     public int getPlayOfNumber() {
         return playOfNumber - 1;
     }
 
+    // Used to add 1 to the number of plays
     public void setPlayOfNumber(){
         this.playOfNumber++;
     }
 
+    // Used to get the personal ride history
     public ArrayList<String> getRideHistories() {
         return rideHistories;
     }
