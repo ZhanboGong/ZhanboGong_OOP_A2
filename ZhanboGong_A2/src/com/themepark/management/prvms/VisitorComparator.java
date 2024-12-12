c5cb698e4a36c09428bbc9f219079bb721cefce1
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-12-12 14:12:52 +0800

    Update visitorComparator.java
    Writing comments

--- ZhanboGong_A2/src/com/themepark/management/prvms/VisitorComparator.java
+++ ZhanboGong_A2/src/com/themepark/management/prvms/VisitorComparator.java
@@ -2,6 +2,9 @@ package com.themepark.management.prvms;
 
 import java.util.Comparator;
 
+/**
+ * The Comparator interface is implemented by overriding the compare method to first check if the ids are the same and, if so, compare lastName
+ */
 public class VisitorComparator implements Comparator<Visitor> {
     @Override
     public int compare(Visitor visitor1, Visitor visitor2) {
